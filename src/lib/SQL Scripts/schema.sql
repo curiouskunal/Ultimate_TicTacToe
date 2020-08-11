f175883a5ccc0ee9e45ce3f872c6a863fa11cff7
Pareek Ravi <pareek96@gmail.com>
2020-08-11 17:19:38 -0400

    Change db table schema

--- src/lib/SQL Scripts/schema.sql
+++ src/lib/SQL Scripts/schema.sql
@@ -1,19 +1,17 @@
--- CREATE TABLE room_numbers (
---     room_number VARCHAR(5) PRIMARY KEY,
---     room_name VARCHAR(252),
---     date_start DATE,
--- );
+CREATE TABLE room_numbers (
+    room_number VARCHAR(5) PRIMARY KEY,
+    room_name VARCHAR(252),
+    date_start DATE,
+);
 
--- CREATE TABLE users (
---     id UNIQUEIDENTIFIER PRIMARY KEY,
---     username VARCHAR
--- );
+CREATE TABLE users (
+    id UNIQUEIDENTIFIER PRIMARY KEY,
+    username VARCHAR(252)
+);
 
--- CREATE TABLE game (
---     room_number VARCHAR(5) PRIMARY KEY,
---     player1_id UNIQUEIDENTIFIER FOREIGN KEY REFERENCES users(id),
---     player2_id UNIQUEIDENTIFIER FOREIGN KEY REFERENCES users(id)
--- );
-
-
-select * from room_numbers;
+CREATE TABLE game (
+    room_number VARCHAR(5) PRIMARY KEY FOREIGN KEY REFERENCES room_numbers(room_number),
+    player1_id UNIQUEIDENTIFIER FOREIGN KEY REFERENCES users(id),
+    player2_id UNIQUEIDENTIFIER FOREIGN KEY REFERENCES users(id),
+    board TEXT NOT NULL
+);
