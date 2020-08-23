db6cb61db515a242a7501745c54353a72cf1718d
Pareek Ravi <pareek96@gmail.com>
2020-08-23 14:45:50 -0400

    Create cron job

--- src/lib/SQL Scripts/schema.sql
+++ src/lib/SQL Scripts/schema.sql
@@ -10,3 +10,12 @@ CREATE TABLE game (
     room_number VARCHAR(5) PRIMARY KEY FOREIGN KEY REFERENCES room_numbers(room_number),
     board TEXT NOT NULL
 );
+
+
+-- CRON JOB QUERY
+-- DELETE from game where room_number in (
+--     SELECT room_number from room_numbers where DATEDIFF(HOUR, date_start, GETDATE())>50
+-- );
+-- DELETE from room_numbers where room_number in (
+--     SELECT room_number from room_numbers where DATEDIFF(HOUR, date_start, GETDATE())>50
+-- );
